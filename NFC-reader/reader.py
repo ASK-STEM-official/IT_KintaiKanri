4eda0c43f2cbd6e1427abf4684941e5420ff5aaa
OMEGA <lightsound2321@gmail.com>
2025-05-06 09:40:32 +0900

    どうしてIDmを変えるんですか？？？

--- NFC-reader/reader.py
+++ NFC-reader/reader.py
@@ -1,46 +1,8 @@
-import time
-from smartcard.System import readers
-from smartcard.Exceptions import NoCardException, CardConnectionException
+import nfc
 
-def to_hex_string(byte_array):
-    return ''.join(f'{b:02X}' for b in byte_array)
+def on_connect(tag):
+    print("Tag UID:", tag.identifier.hex())
+    return True
 
-def main():
-    rlist = readers()
-    if not rlist:
-        print("カードリーダーが見つかりません。")
-        return
-
-    reader = rlist[0]
-    print(f"接続中: {reader}")
-
-    last_idm = None
-
-    while True:
-        try:
-            conn = reader.createConnection()
-            conn.connect()
-
-            command = [0xFF, 0xCA, 0x00, 0x00, 0x00]
-            response, sw1, sw2 = conn.transmit(command)
-
-            if sw1 == 0x90 and sw2 == 0x00:
-                idm = to_hex_string(response)
-                if idm != last_idm:
-                    print("IDm:", idm)
-                    last_idm = idm
-            else:
-                last_idm = None
-
-            conn.disconnect()
-
-        except (NoCardException, CardConnectionException):
-            last_idm = None
-        except Exception as e:
-            print("エラー:", e)
-            last_idm = None
-
-        time.sleep(0.2)
-
-if __name__ == '__main__':
-    main()
+with nfc.ContactlessFrontend('usb') as clf:
+    clf.connect(rdwr={'on-connect': on_connect})
